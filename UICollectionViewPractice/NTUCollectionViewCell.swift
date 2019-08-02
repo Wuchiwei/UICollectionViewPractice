86fd5c5bf74c96504e5a078272b40d73620dd8e0
Luke <wuchiwei17@gmail.com>
2019-08-02 17:52:49 +0800

    Establish UICollectionView.

---
+++ UICollectionViewPractice/NTUCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NTUCollectionViewCell.swift
+//  UICollectionViewPractice
+//
+//  Created by WU CHIH WEI on 2019/7/28.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class NTUCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+}
--- UICollectionViewPractice/ViewController.swift
+++ UICollectionViewPractice/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NTU", for: indexPath)
+        
+        guard let ntuCell = cell as? NTUCollectionViewCell else {
+            return cell
+        }
+        
+        ntuCell.label.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout
+        
+        guard let flowLayout = layout as? UICollectionViewFlowLayout else {
+            
+            return
+        }
+        
+        flowLayout.scrollDirection = .vertical
+        
+        flowLayout.itemSize = CGSize(width: 200, height: 100)
+        
     }
 
 
